2e463c0eabdc5297c906cbfc27d2778c24355640
Benjamin Allen <benjaminallen1985@yahoo.com>
2018-02-26 04:24:42 -0600

    course work

---
+++ PunService.AJAX/PunClient.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PunClient.aspx.cs" Inherits="PunService.AJAX.PunClient" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ScriptManager runat="server">
+            <Services>
+                <asp:ServiceReference Path="~/PunService.svc" />
+            </Services>
+        </asp:ScriptManager>
+        <script type="text/javascript">
+            var service = new PunService();
+            service.GetPuns(function(puns) {
+                var table = document.getElementById("punlist");
+                var output = "fasd";
+                for (var i = 0; i < puns.length; i++) {
+                    output += "<tr><td>" + puns[i].PunID + "</td><td>" +
+                        puns[i].PunID + "</td></tr>";
+                }
+                table.innerHTML = output;
+            }, function () {
+                alert("success");
+            }, function (error) {
+                alert("An error occured: " + error._message);
+            }); 
+        </script>
+        <div>
+            <table id="punlist"></table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ PunService.AJAX/PunService.svc.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+using Data;
+
+namespace PunService.AJAX
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class PunService
+    {
+        private PunDataService _service;
+        
+        public PunService()
+        {
+            _service = new PunDataService();
+        }
+        
+        [WebGet(ResponseFormat = WebMessageFormat.Xml)]
+        public Pun[] GetPuns()
+        {
+            return _service.GetPuns();
+        }
+
+        [OperationContract]
+        [WebGet]
+        public Pun GetPunByID(int punID)
+        {
+            return _service.GetPunById(punID);
+        }
+
+        [OperationContract]
+        [WebInvoke]
+        public void CreatePun(Pun pun)
+        {
+            _service.AddPun(pun);
+        }
+
+        [OperationContract]
+        [WebInvoke]
+        public void UpdatePun(Pun pun)
+        {
+            _service.UpdatePun(pun);
+        }
+
+        [OperationContract]
+        [WebInvoke]
+        public void DeletePun(int id)
+        {
+            _service.DeletePun(id);
+        }
+    }
+}
